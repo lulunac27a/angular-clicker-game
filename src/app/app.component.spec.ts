fbf51b5a66cf3e9abf8082a746370526d7c5d5b4
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-07-12 17:35:23 -0500

    Add display counter text unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,14 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Angular Clicker Game');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Angular Clicker Game',
+    );
+  });
+  it('should display counter text', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('span')?.textContent).toContain('Count: 0');
   });
 });
