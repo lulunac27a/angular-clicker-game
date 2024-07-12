b620f1e53dc850e4afed464c49d74f5d8afd98c9
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-07-12 17:26:22 -0500

    Add new unit tests

--- src/app/clicker-button/clicker-button.component.spec.ts
+++ src/app/clicker-button/clicker-button.component.spec.ts
@@ -19,4 +19,35 @@ describe('ClickerButtonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should initialize values when app is loaded', () => {
+    expect(component.count).toEqual(0);
+    expect(component.count2).toEqual(0);
+    expect(component.count3).toEqual(0);
+  });
+  it('should update counts', () => {
+    component.increaseCount();
+    expect(component.count).toEqual(1);
+    component.changeValue(5);
+    component.increaseCount(true);
+    expect(component.count).toEqual(6);
+    component.increaseCount2(true);
+    expect(component.count2).toEqual(30);
+    component.increaseCount3(true);
+    expect(component.count3).toEqual(150);
+    component.changeValue(3);
+    component.increaseCount3(true);
+    expect(component.count3).toEqual(240);
+  });
+  it('should update non-integer amount values', () => {
+    component.changeValue(3.75);
+    expect(component.amount).toEqual(3);
+  });
+  it('should update amount values greater than 9', () => {
+    component.changeValue(25);
+    expect(component.amount).toEqual(9);
+  });
+  it('should update amount values less than 1', () => {
+    component.changeValue(-7);
+    expect(component.amount).toEqual(1);
+  });
 });
