623ad882bd726e407956e8d8e6d6ace10d3b5e70
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-07-12 16:20:41 -0500

    Fix wrong test assertion

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,8 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, angular-clicker-game',
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Angular Clicker Game');
   });
 });
